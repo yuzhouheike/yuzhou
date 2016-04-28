0806b1bdb44de9ca253d505325f30d35cf216046
liyi <liyi@wangleileideMacBook-Air.local>
2016-04-28 16:16:36 +0800

    Initial Commit

---
+++ LeiLei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeiLei
+//
+//  Created by liyi on 16/4/28.
+//  Copyright © 2016年 liyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeiLei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeiLei
+//
+//  Created by liyi on 16/4/28.
+//  Copyright © 2016年 liyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
